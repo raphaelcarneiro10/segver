963658c7fbca0ebeb3972f05256f49eb3aabcc3f
TÉCNICO EM INFORMÁTICA 2018.1 <36121742018.1@senacrjedu.local>
2018-07-31 09:10:47 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Aula Git</title>
+</head>
+<body>
+	<h1>Arquivo a ser persistido no repositório Git</h1>
+
+
+<?php
+//impressão
+
+echo "<br>Olá Mundo";
+
+
+?>
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
